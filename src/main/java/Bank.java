c1120b98b3da212d8d3b57f2fccba296acd6598a
ethantunstall <et2@illinois.edu>
2018-02-21 12:25:42 -0600

    Completed with some checkstyle

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -8,9 +8,14 @@
  * @see <a href="https://cs125.cs.illinois.edu/lab/8/">Lab 8 Description</a>
  */
 public class Bank {
-
+    /**
+     *
+     */
     public String bankName;
 
+    /**
+     *
+     */
     public Bank() {
         bankName = "Illini Bank";
     }
@@ -26,9 +31,11 @@ public class Bank {
      * @return boolean
      */
     public boolean withdrawMoney(final BankAccount bankAccount, final double amount) {
-        /*
-         * Implement this function
-         */
+        if (bankAccount.getAccountBalance() >= amount) {
+            bankAccount.setAccountBalance(bankAccount.getAccountBalance() - amount);
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -42,9 +49,8 @@ public class Bank {
      * @return boolean
      */
     public boolean depositMoney(final BankAccount bankAccount, final double amount) {
-        /*
-         * Implement this function
-         */
+        bankAccount.setAccountBalance(bankAccount.getAccountBalance() + amount);
+        return true;
     }
 
     /**
@@ -61,10 +67,14 @@ public class Bank {
 
     public boolean transferMoney(final BankAccount source, final BankAccount destination,
             final double amount) {
-        /*
-         * Implement this function
-         */
+        if (withdrawMoney(source, amount)) {
+            withdrawMoney(source, amount);
+            depositMoney(destination, amount);
+            return true;
+        }
+        return false;
     }
+
 
     /**
      * Change back account owner name.
@@ -74,23 +84,30 @@ public class Bank {
      */
 
     public void changeOwnerName(final BankAccount bankAccount, final String name) {
-        /*
-         * Implement this function
-         */
+        if (name != null) {
+            bankAccount.setOwnerName(name);
+        }
     }
 
-    public static int totalAccounts = 0;
+    /**
+     *
+     */
+    private static int totalAccounts = 0;
     /**
      * Uses static variable to get number of bank accounts opened.
      *
      * @return the total number of accounts
      */
     public static int getNumberOfAccount() {
-        /*
-         * Implement this function
-         */
+        return totalAccounts;
     }
 
+    /**
+     *
+     */
+    public static void increment() {
+        totalAccounts++;
+}
     /**
      * Main method for testing.
      *
--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -13,26 +13,121 @@ public class BankAccount {
      * You may want to use this to distinguish between different kinds of accounts.
      */
     public enum BankAccountType {
+        /**
+         *
+         */
         CHECKINGS,
+        /**
+         *
+         */
         SAVINGS,
+        /**
+         *
+         */
         STUDENT,
+        /**
+         *
+         */
         WORKPLACE
     }
 
+    /**
+     *
+     */
     private int accountNumber;
+    /**
+     *
+     */
     public BankAccountType accountType;
+    /**
+     *
+     */
     private double accountBalance;
+    /**
+     *
+     */
     private String ownerName;
+    /**
+     *
+     */
     public double interestRate;
+    /**
+     *
+     */
     private double interestEarned;
 
+    /**
+     *
+     * @param name string
+     * @param accountCategory amount in each category
+     */
     public BankAccount(final String name, final BankAccountType accountCategory) {
-        /*
-         * Implement this function
-         */
+        this.ownerName = name;
+        this.accountType = accountCategory;
+        Bank.increment();
     }
 
-    /*
-     * Implement getters and setters as appropriate for private variables.
+    /**
+     *
+     * @return
      */
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     *
+     * @param accountNumber account number
+     */
+    public void setAccountNumber(final int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    /**
+     *
+     * @return account balance
+     */
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    /**
+     *
+     * @param accountBalance balance
+     */
+    public void setAccountBalance(final double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    /**
+     *
+     * @param ownerName name of owner
+     */
+    public void setOwnerName(final String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    /**
+     *
+     * @return interest earned
+     */
+    public double getInterestEarned() {
+        return interestEarned;
+    }
+
+    /**
+     *
+     * @param interestEarned double
+     */
+    public void setInterestEarned(final double interestEarned) {
+        this.interestEarned = interestEarned;
+    }
 }
